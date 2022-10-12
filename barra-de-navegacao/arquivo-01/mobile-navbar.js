ccdad5726bdd259d9b5c23e9001e55eb51622b7b
leticiadssantos <le.123dasilva@gmail.com>
2022-10-12 20:37:07 -0300

    teste envio de arquivos pelo git bash

---
+++ barra-de-navegacao/arquivo-01/mobile-navbar.js
@@ -0,0 +1,7 @@
+class MobilleNavbar {
+    constructor (mobileMenu){//propripriedades da classe
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelector(mobileMenu);
+    }
+}
