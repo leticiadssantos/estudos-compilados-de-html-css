d2e28026ba4ce498644a1c6a1b7fd48b73e6bafb
Letícia Santos <110691992+leticiadssantos@users.noreply.github.com>
2022-10-12 23:19:44 -0300

    Delete barra-de-navegacao/arquivo-01 directory

--- barra-de-navegacao/arquivo-01/mobile-navbar.js
+++
@@ -1,7 +0,0 @@
-class MobilleNavbar {
-    constructor (mobileMenu){//propripriedades da classe
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelector(mobileMenu);
-    }
-}
